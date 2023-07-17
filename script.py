09c322accea80bc89b9c1dfbb23560f7530c310e
Lunarciez <lunarciez@internet.ru>
2023-07-17 13:50:53 +0300

    renamed and modified main_code.py >> script.py

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ AlreadyNotifiedHigh = False
 
 
 def sound():
-    playsound(r"C:\Users\Lunarciez\PycharmProjects\Low-Battery-Notification\icons\tech-notification-sound.mp3")
+    playsound(r"C:\Users\Lunarciez\PycharmProjects\Low-Battery-Notification\files\notify.mp3")
 
 
 def notification_low():
@@ -40,6 +40,7 @@ while True:
     elif battery.percent >= 80 and is_plugged is True and AlreadyNotifiedHigh is False:
         Thread(target=sound).start()
         Thread(target=notification_high).start()
+        print("battery high notification started.")
         AlreadyNotifiedHigh = True
     elif battery.percent >= 80 and is_plugged is False and AlreadyNotifiedHigh is True:
         AlreadyNotifiedHigh = False
@@ -47,20 +48,27 @@ while True:
     battery = psutil.sensors_battery()
     is_plugged = battery.power_plugged
 
-    print("sleep mode.")
+    print("Sleep mode:")
 
 # making a dependence of the sleep time on the percentage of battery charge
     if 80 <= battery.percent <= 100:
+        print(f"Sleep mode on: {battery.percent}%, time: 14400")
         time.sleep(14400)
     elif 70 <= battery.percent < 80:
+        print(f"Sleep mode on: {battery.percent}%, time: 12200")
         time.sleep(12200)
     elif 50 <= battery.percent < 70:
+        print(f"Sleep mode on: {battery.percent}%, time: 7200")
         time.sleep(7200)
     elif 40 <= battery.percent < 50:
+        print(f"Sleep mode on: {battery.percent}%, time: 2500")
         time.sleep(2500)
     elif 30 <= battery.percent < 40:
+        print(f"Sleep mode on: {battery.percent}%, time: 500")
         time.sleep(500)
     elif 25 <= battery.percent < 30:
+        print(f"Sleep mode on: {battery.percent}%, time: 300")
         time.sleep(300)
     else:
+        print(f"Sleep mode on: {battery.percent}%, time: 100")
         time.sleep(100)
